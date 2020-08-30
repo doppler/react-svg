2860dc9039dacebe00c0ee3650cd6dd0a28af4e2
David Rose <doppler@gmail.com>
2020-08-29 20:27:08 -0400

    got a % guage working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Guage from './components/Guage';
 
 function App() {
+  const [percent, setPercent] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div id='container'>
+        <div id='svg'>
+          <Control percent={percent} setPercent={setPercent} />
+          <Guage percent={percent} />
+        </div>
+      </div>
     </div>
   );
 }
+
+type Props = {
+  percent: number;
+  setPercent: React.Dispatch<React.SetStateAction<number>>;
+};
+const Control = ({ percent, setPercent }: Props) => (
+  <div id='control'>
+    <input
+      type='range'
+      min={0}
+      max={100}
+      value={percent}
+      onChange={e => {
+        setPercent(Number(e.target.value));
+      }}
+    />{' '}
+    {percent}
+  </div>
+);
 
 export default App;
---
+++ src/components/Guage.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+const calcXY = (angle: number, radius = 50, center = 256) => {
+  const radians = (angle * Math.PI) / 180;
+  const x = center + radius * Math.cos(radians);
+  const y = center + radius * Math.sin(radians);
+  return { x, y };
+};
+type Props = {
+  percent: number;
+};
+
+const Guage = ({ percent }: Props) => {
+  const svgWidth = 512,
+    svgHeight = svgWidth,
+    radius = svgWidth / 2;
+
+  const { x: x1, y: y1 } = calcXY(0, radius - 10);
+  const { x: x2, y: y2 } = calcXY(270, radius - 10);
+  const { x: x3, y: y3 } = calcXY(270, radius - 110);
+  const { x: x4, y: y4 } = calcXY(0, radius - 110);
+
+  const { x: lx1, y: ly1 } = calcXY((270 / 100) * percent, radius - 110);
+  const { x: lx2, y: ly2 } = calcXY((270 / 100) * percent, radius - 10);
+
+  return (
+    <svg
+      id='Guage'
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox={`0 0 ${svgWidth} ${svgHeight}`}
+    >
+      <circle
+        id='background-circle'
+        cx={radius}
+        cy={radius}
+        r={radius}
+        fill='hsl(240, 90%, 20%)'
+      />
+      <g transform={`rotate(135, ${radius}, ${radius})`}>
+        <path
+          d={`
+        M ${x1} ${y1} 
+        A ${radius - 10} ${radius - 10} 1 1 1 ${x2} ${y2}
+        L ${x3} ${y3}
+        A ${radius - 110} ${radius - 110} 0 1 0 ${x4} ${y4}
+        Z
+        `}
+          stroke='white'
+        />
+        <line
+          x1={lx1}
+          y1={ly1}
+          x2={lx2}
+          y2={ly2}
+          stroke={`hsl(30, 100%, 50%)`}
+          strokeWidth={5}
+        />
+      </g>
+      <text id='big-text' x={radius} y={radius + 36}>
+        {percent} %
+      </text>
+    </svg>
+  );
+};
+
+export default Guage;
